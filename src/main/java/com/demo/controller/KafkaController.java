15c0cb8929e131d000be1c92eb27daadff4ea2a1
Alexander Lozano <alexanderlozano950@gmail.com>
2025-03-21 01:05:04 -0500

    creacion del readme

---
+++ src/main/java/com/demo/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.services.KafkaProducerService;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaController {
+	
+	@Autowired
+	private KafkaProducerService kafkaProducerService;
+
+	@PostMapping("/send")
+	public String sendMessage(@RequestParam String topic, @RequestParam String message) {
+		kafkaProducerService.sendMessage(topic, message);
+		return "✅ Mensaje enviado correctamente!";
+	}
+}
