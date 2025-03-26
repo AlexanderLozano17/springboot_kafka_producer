e81030fa0d7dcd091ee2300dda4e93f02b64042e
Alexander Lozano <alexanderlozano950@gmail.com>
2025-03-26 02:03:04 -0500

    se adiciona el modulo principal para arrancar el proyecto

---
+++ main-app/src/main/java/com/demo/main/MainApplication.java
@@ -0,0 +1,12 @@
+package com.demo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.demo.core"})
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+}
---
+++ main-app/src/main/java/com/demo/main/controller/ComentarioController.java
@@ -0,0 +1,10 @@
+package com.demo.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/comentario")
+public class ComentarioController {
+
+}
